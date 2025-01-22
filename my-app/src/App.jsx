f074e8bc39dde63f4047259121df4468ed515ac6
KoEunsuk <koeunsuk12@gmail.com>
2025-01-23 01:42:04 +0900

    emotion added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,24 @@
+/** @jsxImportSource @emotion/react */
 import { useState } from 'react'
 import Counter from './components/Counter'
+import { css } from '@emotion/react'
 
+const style = css`
+  color: hotpink;
+`
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+  </div>
+)
 function App() {
   const [totalCount, setTotalCount] = useState(0);
   return (
+    
       <div>
+        <SomeComponent />
         TotalCount: {totalCount}
         <Counter 
         onIncrease={(count) =>{   // 자식컴포넌트에서 인자로 전달되어 count에 저장됨
